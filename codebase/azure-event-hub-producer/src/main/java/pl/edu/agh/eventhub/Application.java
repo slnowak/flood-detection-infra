77ba497e366b3d82521de5865f16e574f75f79a0
Kamil Kulak <kulak.kam@gmail.com>
2017-03-11 16:07:04 +0100

    closing down event hub client

--- codebase/azure-event-hub-producer/src/main/java/pl/edu/agh/eventhub/Application.java
+++ codebase/azure-event-hub-producer/src/main/java/pl/edu/agh/eventhub/Application.java
@@ -6,13 +6,15 @@ import pl.edu.agh.eventhub.producer.EventProducer;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final EventHubClient eventHubClient = EventHubClientFactory.create();
         final EventProducer eventProducer = new EventProducer(eventHubClient);
 
         System.out.println("Producing finite double stream of water measurements..");
         eventProducer.produceFiniteDoubleStreamInRange(3.0d, 7.0d);
         System.out.println("Job finished");
+
+        eventHubClient.onClose().get();
     }
 
 }
